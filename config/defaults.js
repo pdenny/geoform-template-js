0c5f164a522c45f096b1bfcb1409704e51d517a9
Patrick Denny <pdenny@resdat.com>
2018-04-03 07:53:12 -0800

    Configure GeoForm for Driveway permits

--- config/defaults.js
+++ config/defaults.js
@@ -2,19 +2,242 @@ define({
     //Default configuration settings for the applciation. This is where you"ll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
-    "appid": "",
-    "webmap": "0c5cb13c4fc54b28bb26a125221ed96f",
+    "appid": "0212e67f6bba4a37a18b49e5f6e1ec7a",
+    "webmap": "430d8cb4ab77479c88342a933255edf7",
     "form_layer": {
-        "id": ""
+        "id": "Permits_App_3204"
     },
     "details": {
-        "Title": "",
-        "Logo": "",
-        "Description": ""
+      "Title": "Driveway Permit Application",
+      "Logo": "",
+      "Description": "<p>Please fill out each field below.&nbsp; If you have questions or need assistance submitting an application please contact the Permit Center at (907) 861-7822.</p>"
     },
     "fields": {
+            "Permits_App_3204": [
+                {
+                    "name": "PermitType",
+                    "alias": "PermitType",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "dropdown"
+                },
+                {
+                    "name": "Status",
+                    "alias": "Status",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "dropdown"
+                },
+                {
+                    "name": "P_ID",
+                    "alias": "P_ID",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "textbox"
+                },
+                {
+                    "name": "SiteAddress",
+                    "alias": "Site Address",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "AccessOnto",
+                    "alias": "Access Onto",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "DrivewayMarker",
+                    "alias": "Driveway Marker",
+                    "fieldDescription": "If the driveway is already constructed, enter Existing.",
+                    "visible": true,
+                    "displayType": "dropdown"
+                },
+                {
+                    "name": "ExpectedCompletionDate",
+                    "alias": "Expected Completion Date",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "preventPast": false,
+                    "preventFuture": false,
+                    "setCurrentDate": false,
+                    "hiddenDate": false
+                },
+                {
+                    "name": "OwnerName",
+                    "alias": "Owner Name",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "OwnerMailingAddress",
+                    "alias": "Owner Mailing Address",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "OwnerCity",
+                    "alias": "Owner City",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "OwnerState",
+                    "alias": "Owner State",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "OwnerZip",
+                    "alias": "Owner Zip",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "OwnerPhone",
+                    "alias": "Owner Phone",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "OwnerFax",
+                    "alias": "Owner Fax",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "OwnerEmail",
+                    "alias": "Owner Email",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "ApplicantName",
+                    "alias": "Applicant Name",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "ApplicantMailingAddress",
+                    "alias": "Applicant Mailing Address",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "ApplicantCity",
+                    "alias": "Applicant City",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "ApplicantState",
+                    "alias": "Applicant State",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "ApplicantZip",
+                    "alias": "Applicant Zip",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "ApplicantPhone",
+                    "alias": "Applicant Phone",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "ApplicantFax",
+                    "alias": "Applicant Fax",
+                    "fieldDescription": "",
+                    "visible": true
+                },
+                {
+                    "name": "ApplicantEmail",
+                    "alias": "Applicant Email",
+                    "fieldDescription": "",
+                    "visible": true,
+                    "displayType": "text"
+                },
+                {
+                    "name": "FinalApprovalDate",
+                    "alias": "Final Approval Date",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "preventPast": false,
+                    "preventFuture": false,
+                    "setCurrentDate": false,
+                    "hiddenDate": false
+                },
+                {
+                    "name": "TaxAccount",
+                    "alias": "TaxAccount",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "text"
+                },
+                {
+                    "name": "Subdivision",
+                    "alias": "Subdivision",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "text"
+                },
+                {
+                    "name": "RSA",
+                    "alias": "RSA",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "text"
+                },
+                {
+                    "name": "PermitID",
+                    "alias": "PermitID",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "textbox"
+                },
+                {
+                    "name": "OldAppNum",
+                    "alias": "OldAppNum",
+                    "fieldDescription": "",
+                    "visible": false
+                },
+                {
+                    "name": "AppURL",
+                    "alias": "AppURL",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "text"
+                },
+                {
+                    "name": "Comments",
+                    "alias": "Comments",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "text"
+                },
+                {
+                    "name": "GovernPM_ID",
+                    "alias": "GovernPM_ID",
+                    "fieldDescription": "",
+                    "visible": false,
+                    "displayType": "textbox"
+                }
+            ]
     },
-    "theme": "basic", // see values in themes.js
+    "theme": "cerulean", // see values in themes.js
     "oauthappid": null,
     //Enter the url to the proxy if needed by the applcation. See the "Using the proxy page" help topic for details
     // //developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
@@ -27,27 +250,32 @@ define({
     //Defaults to arcgis.com. Set this value to your portal or organization host name.
     "sharinghost": "https://mapportaltst.matsugov.us/arcgis",
     "units": null,
-    "useSmallHeader": false,
-    "enableSharing": true,
-    "defaultMapExtent": true,
-    "pushpinColor": "blue",
+    "useSmallHeader": true,
+    "enableSharing": false,
+    "defaultMapExtent": false,
+    "pushpinColor": "green",
     "nextBasemap": "hybrid",
     "defaultBasemap": "topo",
-    "selectedTitleField": {},
-    "disableViewer": false,
+    "selectedTitleField": {
+	    "Cartegraph_OMS_GIS_Pavement_3337": "name",
+	    "Administrative_CityBoundaries_2158": "CITYNAME",
+	    "PublicSafety_Addresses_209": "ACCOUNT",
+	    "Permits_App_3204": "OwnerName"
+    },
+    "disableViewer": true,
     "enableAttachments": true,
     "attachmentIsRequired": false,
     "attachmentLabel": "",
     "attachmentHelpText": "",
     "showLayer": true,
-    "disableLogo": false,
-    "enableBasemapToggle": false,
-    "enableOfflineSupport": true,
-    "locate":false,
+    "disableLogo": true,
+    "enableBasemapToggle": true,
+    "enableOfflineSupport": false,
+    "locate": true,
     "locationSearchOptions": {
         "enableMyLocation": true,
         "enableSearch": true,
-        "enableLatLng": true,
+        "enableLatLng": false,
         "enableUSNG": false,
         "enableMGRS": false,
         "enableUTM": false
@@ -60,8 +288,14 @@ define({
         "fields": []
     }],
     "attachmentInfo":{
+      "Permits_App_3204": {
+        "enableAttachments": true,
+        "attachmentIsRequired": false,
+        "attachmentLabel": "",
+        "attachmentHelpText": ""
+	    }
     },
-    "submitButtonText": "",
+    "submitButtonText": "Submit Application",
     "viewSubmissionsText": "",
     "helperServices": {
         "geometry": {
